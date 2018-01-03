a7ee2d90e45716dfb3e4d9dd59624c457df80592
Martin Westergaard Lassen <martin@mwl.dk>
2018-01-03 23:19:21 +0100

    Removed redundant configs

--- src/main/java/com/humio/bridges/syslogbridge/model/HumioConfig.java
+++ src/main/java/com/humio/bridges/syslogbridge/model/HumioConfig.java
@@ -12,28 +12,4 @@ public class HumioConfig {
      * Humio host and protocol, i.e. https://cloud.humio.com
      */
     private String urlPrefix;
-    /**
-     * Humio dataspace
-     */
-    private String dataspace;
-
-    /**
-     * TCP port for accepting syslog
-     */
-    private int syslogPort;
-    /**
-     * Humio ingesttoken
-     */
-    private String ingesttoken;
-
-    /**
-     * Number of executors that will take lines off the tcp buffer and convert them to Messages
-     */
-    private int syslogExecutors;
-    /**
-     * Number of executors that will take batches of events and post them to Humio
-     */
-    private int httpExecutors;
-
-    private List<String> grokPaths;
 }
